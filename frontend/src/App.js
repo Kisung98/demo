a94ccb923ff5db21201e398d41c1e4698912efe8
choikisung <choikisung@kisung>
2024-08-24 15:53:29 +0900

    최초푸시

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>안녕하세요 변경함 씨발롬아 씨발 영미야 사랑해</h1>
+    </div>
+  );
+}
+
+export default App;
